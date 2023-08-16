c7739cbaca99426ae77caa0a2f0076a6005f7b5b
Swetha S <110710815+Swetha5021@users.noreply.github.com>
2023-08-16 20:36:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +22,7 @@ function App() {
       </header>
     </div>
   );
+  
 }
 
 export default App;
